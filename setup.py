290ed151187c07d921cd120a2e75e12619e17597
Mac <makarand@theprocedure.in>
2018-07-13 21:12:44 +0530

    Bump version: 0.2.7 → 0.2.8

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/the-procedure/textlocal_python',
-    version='0.2.7',
+    version='0.2.8',
     zip_safe=False,
 )
